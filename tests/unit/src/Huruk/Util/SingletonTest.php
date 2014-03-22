c1308536525c86e17970aba605b252a96a216b2e
Miguel Pedregosa <miguelpedregosa@gmail.com>
2014-03-22 13:15:32 +0100

    Initial setup

---
+++ tests/unit/src/Huruk/Util/SingletonTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 
+ * User: migue
+ * Date: 22/03/14
+ * Time: 13:15
+ */
+
+namespace unit\src\Huruk\Util;
+
+
+class SingletonTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testSingleton ()
+    {
+
+    }
+
+}
