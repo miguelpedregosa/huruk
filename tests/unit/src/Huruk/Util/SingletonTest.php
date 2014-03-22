b9804d36d39c1fb1ca7edb0b2b193b94afbce221
Miguel Pedregosa <miguelpedregosa@gmail.com>
2014-03-22 13:48:41 +0100

    Initial setup

--- tests/unit/src/Huruk/Util/SingletonTest.php
+++ tests/unit/src/Huruk/Util/SingletonTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 
+ *
  * User: migue
  * Date: 22/03/14
  * Time: 13:15
@@ -12,9 +12,9 @@ namespace unit\src\Huruk\Util;
 class SingletonTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testSingleton ()
+    public function testSingleton()
     {
-
+        $this->assertEquals(1, 1);
     }
 
 }
