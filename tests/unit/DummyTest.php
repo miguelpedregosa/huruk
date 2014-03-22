75f76e831edc5d2ae24e64f6010c919b5565172a
Miguel Pedregosa <miguelpedregosa@gmail.com>
2014-03-22 10:46:47 +0100

    Initial setup

---
+++ tests/unit/DummyTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * User: migue
+ * Date: 22/03/14
+ * Time: 10:46
+ */
+
+namespace unit;
+
+
+class DummyTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+
+}
+ 
