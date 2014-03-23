6aee8f27a96b342c22727c157c8496cbbd2aaf51
Miguel Pedregosa <miguelpedregosa@gmail.com>
2014-03-23 10:28:25 +0100

    SIngleton clone test

--- tests/unit/src/Huruk/Util/SingletonTest.php
+++ tests/unit/src/Huruk/Util/SingletonTest.php
@@ -12,9 +12,22 @@ use unit\src\Huruk\Util\sut\DummySingleton;
 
 class SingletonTest extends \PHPUnit_Framework_TestCase
 {
+    private $errors;
+
     public static function setupBeforeClass()
     {
         require_once __DIR__ . '/sut/DummySingleton.php';
+    }
+
+    public function errorHandler($errno, $errstr, $errfile, $errline, $errcontext)
+    {
+        $this->errors[] = compact(
+            "errno",
+            "errstr",
+            "errfile",
+            "errline",
+            "errcontext"
+        );
     }
 
     /**
@@ -37,6 +50,38 @@ class SingletonTest extends \PHPUnit_Framework_TestCase
         $new_dummy = DummySingleton::getInstance(true);
         $this->assertInstanceOf('unit\src\Huruk\Util\sut\DummySingleton', $new_dummy);
         $this->assertNull($new_dummy->getValue());
+    }
+
+    public function testClone ()
+    {
+        /** @var DummySingleton $dummy */
+        $dummy = DummySingleton::getInstance();
+        $dummy->setValue(10);
+        $new_dummy = clone $dummy;
+        $this->assertError("Clone not allowed.", E_USER_ERROR);
+        $this->assertInstanceOf('unit\src\Huruk\Util\sut\DummySingleton', $new_dummy);
+    }
+
+    public function assertError($errstr, $errno)
+    {
+        foreach ($this->errors as $error) {
+            if ($error["errstr"] === $errstr
+                && $error["errno"] === $errno
+            ) {
+                return;
+            }
+        }
+        $this->fail(
+            "Error with level " . $errno .
+            " and message '" . $errstr . "' not found in ",
+            var_export($this->errors, true)
+        );
+    }
+
+    protected function setUp()
+    {
+        $this->errors = array();
+        set_error_handler(array($this, "errorHandler"));
     }
 
 }
