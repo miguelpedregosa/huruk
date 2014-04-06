cbc38247a5ec54c5aee682b83696f2ef078adc7a
migue <miguelpedregosa@gmail.com>
2014-04-06 15:05:49 +0200

    Services Factory

--- tests/unit/src/Huruk/Services/ServicesFactoryTest.php
+++ tests/unit/src/Huruk/Services/ServicesFactoryTest.php
@@ -42,4 +42,36 @@ class ServicesFactoryTest extends \PHPUnit_Framework_TestCase
         $service_again = ServicesFactory::getService('dummy');
         $this->assertEquals(125, $service_again->getValue());
     }
+
+    /**
+     *
+     */
+    public function testReRegisterService()
+    {
+        ServicesFactory::registerService(
+            'dummy',
+            function () {
+                return new DummyService();
+            }
+        );
+
+        /** @var \unit\src\Huruk\Services\sut\DummyService $service */
+        $service = ServicesFactory::getService('dummy');
+        $this->assertInstanceOf('\unit\src\Huruk\Services\sut\DummyService', $service);
+
+        $service->setValue(125);
+        $this->assertEquals(125, $service->getValue());
+
+        ServicesFactory::registerService(
+            'dummy',
+            function () {
+                return new DummyService();
+            }
+        );
+
+        /** @var \unit\src\Huruk\Services\sut\DummyService $service */
+        $service = ServicesFactory::getService('dummy');
+        $this->assertNotEquals(125, $service->getValue());
+        $this->assertNull($service->getValue());
+    }
 }
